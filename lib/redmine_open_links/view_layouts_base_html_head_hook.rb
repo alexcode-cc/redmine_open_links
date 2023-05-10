2995338b51d4370a0ae128a4a801e20298287c13
Alex Codeing <alexcode.cc@outlook.com>
2023-05-10 14:31:41 +0800

    fix: module name error

--- lib/redmine_open_links/view_layouts_base_html_head_hook.rb
+++ lib/redmine_open_links/view_layouts_base_html_head_hook.rb
@@ -1,4 +1,4 @@
-module RedmineOpenLinksInNewWindow
+module RedmineOpenLinks
   class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = {})
       javascript_include_tag('redmine_open_links.js', :plugin => :redmine_open_links)
